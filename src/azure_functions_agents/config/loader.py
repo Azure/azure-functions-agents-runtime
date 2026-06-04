63292d0a9c66717677c4ce5d9283318c79556f3c
Victoria Hall <victoriahall@microsoft.com>
2026-06-04 11:11:38 -0500

    lint3

--- src/azure_functions_agents/config/loader.py
+++ src/azure_functions_agents/config/loader.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from typing import Any, cast
 
 import frontmatter
-import yaml
+import yaml  # type: ignore[import-untyped]
 from pydantic import ValidationError
 
 from azure_functions_agents._logger import logger
