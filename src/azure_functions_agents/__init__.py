d809ff9b385cb02f62a2ff9b7d91ae01861c51eb
AzureFunctions <azurefunctions@microsoft.com>
2026-06-05 15:08:37 +0000

    build: update Azure Functions Agent Runtime version to 0.1.0b3

--- src/azure_functions_agents/__init__.py
+++ src/azure_functions_agents/__init__.py
@@ -13,7 +13,7 @@ Framework. The most common entry points are:
   as agent tools.
 """
 
-__version__ = "0.0.0a2"
+__version__ = "0.1.0b3"
 
 # ---------------------------------------------------------------------------
 # Global MAF ExperimentalWarning suppression
